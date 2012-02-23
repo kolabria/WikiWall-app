548456f6c9053f011c936e22f9cefdc556d9da89
Alok Mohindra <alok.mohindra@gmail.com>
2012-02-22 22:19:40 -0500

    enabled admin at /admin

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ from login import views
 
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 #    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
@@ -20,8 +20,8 @@ urlpatterns = patterns('',
    
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
