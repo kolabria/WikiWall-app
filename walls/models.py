cfc39825c89e8823a2300f7726f16d3772ab9ec2
Alok Mohindra <alok.mohindra@gmail.com>
2012-02-23 21:17:32 -0500

    added basic Walls models

--- walls/models.py
+++ walls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Walls(models.Model):
+    STATUS_CHOICES = (
+        (u'Active', u'Active'),
+        (u'Private', u'Private'),
+        (u'Inactive', u'Inactive'),
+    )        
+    name = models.CharField(max_length=30)
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    owner = models.ForeignKey(User)
+
