2ae6b7396f6ab04c6466a4e9a8f6f0ca4f9cb804
Alok Mohindra <alok.mohindra@gmail.com>
2012-02-23 21:26:11 -0500

    added wall model to admin; renamed models.Walls to models.Wall

--- walls/models.py
+++ walls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Walls(models.Model):
+class Wall(models.Model):
     STATUS_CHOICES = (
         (u'Active', u'Active'),
         (u'Private', u'Private'),
