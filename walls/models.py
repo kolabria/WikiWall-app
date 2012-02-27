6dee5a90cd9ff194aee23d7f208fc67067dcbf35
Alok Mohindra <alok.mohindra@gmail.com>
2012-02-27 18:19:26 -0500

    updated walls/models.py last_modified default=True

--- walls/models.py
+++ walls/models.py
@@ -12,4 +12,4 @@ class Wall(models.Model):
     owner = models.ForeignKey(User)
     created_on = models.DateField(blank=True,default=True,auto_now_add=True) # the date this wall was first created
 #    last_viewed = models.DateField() # last time this user opened this wall
-    last_modified = models.DateField(blank=True,default=False,auto_now=True) # last time a change was made by any user
+    last_modified = models.DateField(blank=True,default=True,auto_now=True) # last time a change was made by any user
