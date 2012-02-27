2a6d320da094e79c4df8a2ee969192449998994d
Alok Mohindra <alok.mohindra@gmail.com>
2012-02-27 14:57:07 -0500

    added created_on and last_modified date functions to Wall models

--- walls/models.py
+++ walls/models.py
@@ -10,4 +10,6 @@ class Wall(models.Model):
     name = models.CharField(max_length=30)
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     owner = models.ForeignKey(User)
-
+    created_on = models.DateField(blank=True,default=True,auto_now_add=True) # the date this wall was first created
+#    last_viewed = models.DateField() # last time this user opened this wall
+    last_modified = models.DateField(blank=True,default=False,auto_now=True) # last time a change was made by any user
