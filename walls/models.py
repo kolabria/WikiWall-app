1da59556e6a20351aa529dd1bf2967aec8d6502a
Alok Mohindra <alok.mohindra@gmail.com>
2012-02-27 23:17:39 -0500

    added link and set_link method to walls to generate unique url link; added migrations for same

--- walls/models.py
+++ walls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from datetime import datetime
+from datetime import datetime
+from hashlib import md5
 
 class Wall(models.Model):
     STATUS_CHOICES = (
@@ -11,12 +12,29 @@ class Wall(models.Model):
     name = models.CharField(max_length=30)
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     owner = models.ForeignKey(User)
-#    created_on = models.DateField(default=datetime.now) # the date this wall was first created
-#    last_modified = models.DateField(default=datetime.now) # last time a change was made by any user
-#    last_viewed = models.DateField() # last time this user opened this wall
-#
+    link = models.CharField(default='dummylink', max_length=32)
+    
+#    created_on = models.DateTimeField(blank=True, null=True) # timestamp of wall creation 
+#    last_modified = models.DateTimeField(blank=True, null=True) # last change made by any user
+#    last_viewed = models.DateTimeField(blank=True, null=True) # last time this user opened this wall
+
     def __unicode__(self):
         """
         Returns the Wall Name as unicode description for admin and shell
         """
         return self.name
+
+    def set_link(self, **kwargs):
+        raw_string = self.owner.email + str(self.id)
+        link = md5(raw_string).hexdigest()
+        self.link = link
+
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.set_link() # create unique link from hash of email + id
+#            now = datetime.now()
+#            self.created_on = now
+#            self.last_modified = now
+#            self.last_viewed = now
+        super(Wall, self).save(*args, **kwargs)
