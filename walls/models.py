3186e017d97a4d7f7644c1dd5b4c407e3539053d
Alok Mohindra <alok.mohindra@gmail.com>
2012-02-27 20:34:07 -0500

    disabled datetimestamp functionality from Wall model; added unicode name method to Wall model

--- walls/models.py
+++ walls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+#from datetime import datetime
 
 class Wall(models.Model):
     STATUS_CHOICES = (
@@ -10,6 +11,12 @@ class Wall(models.Model):
     name = models.CharField(max_length=30)
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     owner = models.ForeignKey(User)
-    created_on = models.DateField(blank=True,default=True,auto_now_add=True) # the date this wall was first created
+#    created_on = models.DateField(default=datetime.now) # the date this wall was first created
+#    last_modified = models.DateField(default=datetime.now) # last time a change was made by any user
 #    last_viewed = models.DateField() # last time this user opened this wall
-    last_modified = models.DateField(blank=True,default=True,auto_now=True) # last time a change was made by any user
+#
+    def __unicode__(self):
+        """
+        Returns the Wall Name as unicode description for admin and shell
+        """
+        return self.name
