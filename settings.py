6a52e1d0a86a8cc15fcf374f7956351fe6eaee8b
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-08 18:17:13 -0500

    installed and configured django_debug_toolbar_mongo -- mongo debugging panel

--- settings.py
+++ settings.py
@@ -26,6 +26,18 @@ DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': True,
 }
 
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar_mongo.panel.MongoDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+#    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
 # Email Configuration for password recovery, etc.
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = "587"
@@ -41,7 +53,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'debug_toolbar',
-#    'debug_toolbar_mongo',
+    'debug_toolbar_mongo',
 )
 
 INTERNAL_IPS = ('127.0.0.1',)
