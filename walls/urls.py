de959cf5e9cea0d1621af9273dc5534e733ac348
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-10 03:12:23 -0500

    added working links from mywall to users walls

--- walls/urls.py
+++ walls/urls.py
@@ -5,7 +5,7 @@ import views
 
 urlpatterns = patterns('',
     url(r'^walls/$', views.walls),
-    url(r'^walls/<wid>$', views.view_wall),
+    url(r'^walls/(?P<wid>\w+)/$', views.view_wall),
 
     url(r'^create-wall/$', views.create_wall),
     url(r'^config-wall/$', views.config_wall),
