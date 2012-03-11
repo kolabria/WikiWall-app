4353a64281ed32dcc872e13fa2a2f3345d53c87d
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-11 16:18:27 -0400

    added /modal view to prototype modal wall creation popup

--- walls/urls.py
+++ walls/urls.py
@@ -13,4 +13,6 @@ urlpatterns = patterns('',
 
     url(r'^idwall/$', views.idwall),
     url(r'^thewall/$', views.thewall),
+    url(r'^modal/$', views.modal),
+    url(r'^new-wall/$', views.new_wall),
 )
--- walls/views.py
+++ walls/views.py
@@ -9,6 +9,49 @@ from kolabria.walls.forms import NewWallForm
 
 
 @login_required
+def modal(request):
+    # Generate New Wall Form logic but hide form behind modal
+    if request.method == 'GET':
+        form = NewWallForm()
+    else:  # request.method == 'POST'
+        form = NewWallForm(request.POST)
+        if form.is_valid():
+            new_wall = Wall.objects.create(owner=request.user,
+                                           name=request.POST['name'])
+            new_wall.save()
+            data = {'title': 'Kolabria', 'wall': new_wall,}
+            return render_to_response('walls/created.html', data,
+                              context_instance=RequestContext(request))
+
+    # Get walls page with modal pop-up
+    walls = Wall.objects.filter(owner=request.user)
+    data = {'title': 'Kolabria', 
+            'form': form,
+            'walls': walls, }
+    return render_to_response('walls/modal.html', data,
+                              context_instance=RequestContext(request))
+
+
+@login_required
+def new_wall(request):
+    if request.method == 'GET':
+        form = NewWallForm()
+    else:
+        form = NewWallForm(request.POST)
+        if form.is_valid():
+            new_wall = Wall.objects.create(owner=request.user,
+                                           name=request.POST['name'])
+            new_wall.save()
+            data = {'title': 'Kolabria', 'wall': new_wall,}
+            return render_to_response('walls/created.html', data,
+                              context_instance=RequestContext(request))
+
+    data = {'title': 'Kolabria', 'form': form }
+    return render_to_response('walls/modal.html', data,
+                              context_instance=RequestContext(request))
+
+
+@login_required
 def walls(request):
     # Get walls for this user
     walls = Wall.objects.filter(owner=request.user)
