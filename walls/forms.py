1b7f6e757611460f54e24f47a3324b15b12ac082
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-11 18:37:54 -0400

    made create_wall input description into a long description field

--- walls/forms.py
+++ walls/forms.py
@@ -4,7 +4,7 @@ from mongoengine.django.auth import User
 
 class NewWallForm(forms.Form):
     name = forms.CharField(max_length=30, required=True)
-    description = forms.CharField(max_length=256, required=False)
+    description = forms.CharField(widget=forms.Textarea, max_length=256, required=False)
 
 
 class EditWallForm(forms.Form):
