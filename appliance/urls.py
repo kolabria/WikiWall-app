64cc1e6ed4f02a1323c35d7a1b7e20e4c7db298e
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-14 18:09:05 -0400

    added basic appliance generic view

--- appliance/urls.py
+++ appliance/urls.py
@@ -3,13 +3,13 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-            (r'^appliance/$', login_required(TemplateView.as_view(
-                              template_name="appliance/myappliances.html"))),
+    (r'^appliances/$', login_required(TemplateView.as_view(
+                         template_name="appliance/myappliances.html"))),
+#    (r'^details/(?P<box>\w+)/$', login_required(TemplateView.as_view()),
 )
 
 """
 urlpatterns = patterns('appliance',
-    url(r'^detail/(?P<box>\w+)/$', views.detail), 
     url(r'^register/(?P<box>\w+)/$', views.register),
     url(r'^edit/(?P<box>\w+)/$', views.edit),
     url(r'^remove/(?P<box>\w+)/$', views.remove),
