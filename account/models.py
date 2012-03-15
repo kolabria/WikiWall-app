f057c44b5fbaab7ef6751cfa6bbc7c8cf647d7ec
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-15 19:18:28 -0400

    added basic master corporate account -- Kolabria -- to house all user accounts under a single company for dev

--- account/models.py
+++ account/models.py
@@ -2,31 +2,29 @@ from mongoengine import connect, Document
 from mongoengine import ReferenceField, StringField, DateTimeField
 from mongoengine.django.auth import User
 from datetime import datetime
-from kolabria.account.models import Account
 
-class Wall(Document):
+class Account(Document):
     """
-    Wall model in MongoDB to represent Wall objects
+    Corporate Account model in MongoDB to represent Company objects
     """
     STATUS_CHOICES = (
         (u'Active', u'Active'),
-        (u'Private', u'Private'),
         (u'Inactive', u'Inactive'),
-    )        
-    company = ReferenceField(Account, 
-                             default=Account.objects.get(company='Kolabria'))
+    )
+
     owner = ReferenceField(User)
-    name = StringField(max_length=32, required=True)
-    description = StringField(max_length=256, required=False)
+    company= StringField(max_length=32)
+    address1 = StringField(max_length=256)
+    city = StringField(max_length=64) 
     status = StringField(default='Active', 
                          max_length=8, 
                          choices=STATUS_CHOICES,
                          required=True)
-    created = DateTimeField(default=datetime.now(), required=True)
+    created = DateTimeField(default=datetime.now())
     modified = DateTimeField(default=datetime.now(), required=True)
 
     def __unicode__(self):
         """
         Returns the Wall Name as unicode description for admin and shell
         """
-        return self.name
+        return self.company
