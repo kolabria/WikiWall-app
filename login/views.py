63d54fe2230e3a48346a428062955b70d0e840df
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-15 16:56:21 -0400

    updated registration view with view and form logic for mongoDB forms

--- login/views.py
+++ login/views.py
@@ -5,10 +5,10 @@ from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, logout, login
 
-from mongoengine.django import auth
 from mongoengine.django.auth import User
 
-from kolabria.login.forms import LoginForm, RegistrationForm
+#from kolabria.login.forms import LoginForm, RegistrationForm
+from kolabria.login.forms import UserCreationForm
 from kolabria.walls.models import Wall
 
 def public(request):
@@ -17,51 +17,20 @@ def public(request):
     else:
         return HttpResponseRedirect('/login')
 
-def login_user(request):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect('/walls')
-    if request.method == 'GET':
-        form = LoginForm()
-    else:
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user_name = request.POST['username']
-            pass_word = request.POST['password']
-            user = authenticate(username=user_name, password=pass_word)
-            login(request=request, user=user)            
-            return HttpResponseRedirect('/walls')
-    
-    data = {'title': 'Kolabria - Login Page',
-            'form': form,}
-    return render_to_response('login/login.html', data,
-                              context_instance=RequestContext(request))
-
-@login_required
-def loggedin(request):
-    return render_to_response('login/loggedin.html',
-                              context_instance=RequestContext(request))
-
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect('/login')
-
 def register(request):
-    if request.method == 'GET':
-        form = RegistrationForm()
-        data = {'title': 'Kolabria - Registration Page',
-                'form': form,}
-        return render_to_response('login/register.html', data,
-                                  context_instance=RequestContext(request))
-    else:
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user_name = request.POST['username']
-            email = request.POST['email']
-            pass_word = request.POST['password1']
-            new_user = User.create_user(username=user_name, email=email,
-                                        password=pass_word)
-            new_user.save()
-            auth_user = authenticate(username=user_name, password=pass_word)
-            login(request=request, user=auth_user)
-            return render_to_response('login/register-success.html',
+    form = UserCreationForm(request.POST or None)
+    data = {'title': 'Kolabria - Registration Page',
+            'form': form,}
+    if form.is_valid():
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password2']
+        new_user = User.create_user(username=username, email=email,
+                                    password=password)
+        new_user.save()
+        auth_user = authenticate(username=user_name, password=pass_word)
+        login(request=request, user=auth_user)
+        return render_to_response('login/register-success.html',
+                          context_instance=RequestContext(request))
+    return render_to_response('login/register.html', data,
                               context_instance=RequestContext(request))
