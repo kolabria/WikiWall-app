48913bb7d53928ead212fcd4a4e73eb60d1bdd16
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-15 15:01:27 -0400

    updated login and logout functionality using login and logout from django.contrib.auth.views -- better error handling

--- login/urls.py
+++ login/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import include, patterns, url
 from django.conf import settings
-
+from django.contrib.auth.views import login, logout
 import views 
 
 urlpatterns = patterns('',
     url(r'^$', views.public),
     url(r'^public/$', views.public),
-    url(r'^login/$', views.login_user),
-    url(r'^loggedin/$', views.loggedin),
-    url(r'^logout/$', views.logout_user),
+    url(r'^login/$', login, {'template_name': 'login/login.html'},
+                            name='log-in'),
+    url(r'^logout/$', logout, {'next_page': '/'}, name='log-out'),
     url(r'^register/$', views.register),
 )
