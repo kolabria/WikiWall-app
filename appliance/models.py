36832b8cf21a0338fea9483f0900023c700faec9
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-16 14:44:16 -0400

    added company and walls relations to Box model

--- appliance/models.py
+++ appliance/models.py
@@ -2,6 +2,8 @@ from mongoengine import connect, Document
 from mongoengine import ReferenceField, StringField, DateTimeField
 from mongoengine.django.auth import User
 from datetime import datetime
+from kolabria.account.models import Account
+from kolabria.walls.models import Wall
 
 class Box(Document):
     """
@@ -14,6 +16,7 @@ class Box(Document):
         (u'Disabled', u'Disabled'),
     )
 
+    company = ReferenceField(Account)
     owner = ReferenceField(User)
     name = StringField(default='New Appliance', max_length=32, required=True)
     location = StringField(max_length=100, required=False)
@@ -21,6 +24,7 @@ class Box(Document):
                          max_length=16,
                          choices=STATUS_CHOICES,
                          required=True)
+    walls = ListField(Wall(unique=True))
     activated = DateTimeField(default=datetime.now(), required=True)
     modified = DateTimeField(default=datetime.now(), required=True)
 
