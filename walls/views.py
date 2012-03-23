4e344d7aff35174f139a6fa798171cbbdc29b149
Alok Mohindra <alok.mohindra@gmail.com>
2012-03-23 15:11:01 -0400

    on update wall view, update posts data and now redirects to same page

--- walls/views.py
+++ walls/views.py
@@ -255,7 +255,7 @@ def update_wall(request, wid):
         messages.info(request, 'invited: %s | %s | %s ' % (invited, 
                                                            type(invited), 
                                                            len(invited)))
-        return HttpResponseRedirect('/walls/')
+        return HttpResponseRedirect('/walls/update/%s' % wid)
 
     update_form.initial['name'] = wall.name
     update_form.fields['name'].label = 'WikiWall Name'
