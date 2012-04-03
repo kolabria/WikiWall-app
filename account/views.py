757e34d7552740715cddc4d7c530e044adfa1048
Alok Mohindra <alok.mohindra@gmail.com>
2012-04-03 18:06:52 -0400

    updated create account forms and views for -- create a corporate account with billing info

--- account/views.py
+++ account/views.py
@@ -16,10 +16,28 @@ def create(request):
     form = NewAccountForm(request.POST or None)
     if form.is_valid():
         messages.success(request, request.POST)
-        return HttpResponseRedirect('/walls/')
+        return HttpResponseRedirect('/account/create/')
 
     data = {'title': 'Kolabria - Create a new Account ', 'form': form, }
-    return render_to_response('account/register.html', data,
+    return render_to_response('account/create.html', data,
                               context_instance=RequestContext(request))
 
-
+"""
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    data = {'title': 'Kolabria - Registration Page',
+            'form': form,}
+    if form.is_valid():
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password2']
+        new_user = User.create_user(username=username, email=email,
+                                    password=password)
+        new_user.save()
+        auth_user = authenticate(username=username, password=password)
+        login(request=request, user=auth_user)
+        return render_to_response('login/register-success.html',
+                          context_instance=RequestContext(request))
+    return render_to_response('login/register.html', data,
+                              context_instance=RequestContext(request))
+"""
