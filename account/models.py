440bee5b092f530329b504e7285b5931bb402af3
Alok Mohindra <alok.mohindra@gmail.com>
2012-04-03 18:04:31 -0400

    updated account model to include full billing details of corporate account

--- account/models.py
+++ account/models.py
@@ -1,3 +1,5 @@
+from django.contrib.localflavor.us.forms import USPhoneNumberField
+from django.contrib.localflavor.us.forms import USStateField
 from mongoengine import connect, Document
 from mongoengine import ReferenceField, StringField, DateTimeField
 from mongoengine.django.auth import User
@@ -12,10 +14,15 @@ class Account(Document):
         (u'Inactive', u'Inactive'),
     )
 
-    owner = ReferenceField(User)
-    company= StringField(max_length=32)
-    address1 = StringField(max_length=256)
-    city = StringField(max_length=64) 
+    admin = ReferenceField(User)
+    company = StringField(max_length=32)
+    phone = USPhoneNumberField()
+    address1 = StringField(max_length=80)
+    address2 = StringField(max_length=80)
+    city = StringField(max_length=64)
+    state_or_province = USStateField()
+    postal_zip = StringField(max_length=6)
+    country = StringField(max_length=30)
     status = StringField(default='Active', 
                          max_length=8, 
                          choices=STATUS_CHOICES,
